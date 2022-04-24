ce7426a1aac2f8a063285e243339f69275fe17bf
Abián Izquierdo <abizmo@gmail.com>
2022-04-24 12:38:47 +0100

    Add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+
+import { ReactComponent as Delete } from './assets/icons/delete.svg';
+import { ReactComponent as Edit } from './assets/icons/edit.svg';
+import { ReactComponent as Reply } from './assets/icons/reply.svg';
+import Button from './components/Button';
 import Theme from './theme';
 
 function App() {
   return (
     <Theme>
       <h1>Interactive comments section</h1>
+      <Button
+        color="primary"
+        icon={Reply}
+        label="Reply"
+        onClick={console.log}
+      />
+      <Button
+        color="secondary"
+        icon={Delete}
+        label="Delete"
+        onClick={console.log}
+      />
+      <Button
+        color="primary"
+        icon={Edit}
+        label="Edit"
+        onClick={console.log}
+      />
+      <Button
+        color="primary"
+        label="Send"
+        onClick={console.log}
+        size="small"
+        variant="contained"
+      />
+      <Button
+        label="No, cancel"
+        onClick={console.log}
+        size="big"
+        variant="contained"
+      />
+      <Button
+        color="secondary"
+        label="Yes, delete"
+        onClick={console.log}
+        size="big"
+        variant="contained"
+      />
     </Theme>
   );
 }
---
+++ src/components/Button/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+const width = {
+  big: '8.75rem',
+  small: '6.5rem',
+};
+
+export const Btn = styled.button`
+  align-items: center;
+  background: transparent;
+  border: none;
+  color: ${({ color, theme }) => theme.colors[`${color}-500`]};
+  cursor: pointer;
+  display: flex;
+  font-size: ${({ theme }) => theme.fontSizes[500]};
+  font-weight: 500;
+  gap: .5rem;
+  height: 3rem;
+  justify-content: center;
+  line-height: 1.1;
+  padding: 0;
+`;
+
+export const BtnContained = styled(Btn)`
+  background: ${({ color, theme }) => theme.colors[`${color}-500`]};
+  border-radius: .5rem;
+  color: ${({ theme }) => theme.colors['neutral-100']};
+  font-size: ${({ theme }) => theme.fontSizes[600]};
+  padding: .5rem 1rem;
+  text-transform: uppercase;
+  width: ${({ size }) => (size !== 'fit' ? width[size] : 'fit-content')};
+`;
