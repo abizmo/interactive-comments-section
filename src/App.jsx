6ba2cc311159501ca257806d7406a310ce3700ee
Abián Izquierdo Montesdeoca <abizmo@gmail.com>
2022-04-25 11:55:18 +0100

    Update App.jsx
    Change console.log to doNothing when click a button

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as Reply } from './assets/icons/reply.svg';
 import Button from './components/Button';
 import Theme from './theme';
 
+function doNothing() {};
+
 function App() {
   return (
     <Theme>
@@ -14,37 +16,37 @@ function App() {
         color="primary"
         icon={Reply}
         label="Reply"
-        onClick={console.log}
+        onClick={doNothing}
       />
       <Button
         color="secondary"
         icon={Delete}
         label="Delete"
-        onClick={console.log}
+        onClick={doNothing}
       />
       <Button
         color="primary"
         icon={Edit}
         label="Edit"
-        onClick={console.log}
+        onClick={doNothing}
       />
       <Button
         color="primary"
         label="Send"
-        onClick={console.log}
+        onClick={doNothing}
         size="small"
         variant="contained"
       />
       <Button
         label="No, cancel"
-        onClick={console.log}
+        onClick={doNothing}
         size="big"
         variant="contained"
       />
       <Button
         color="secondary"
         label="Yes, delete"
-        onClick={console.log}
+        onClick={doNothing}
         size="big"
         variant="contained"
       />
