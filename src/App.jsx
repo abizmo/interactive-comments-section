b4eb00b037e94c3c76e5348b05ebe26f414ed394
Abián Izquierdo <abizmo@gmail.com>
2022-04-26 21:47:15 +0100

    Update App to show comment component

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { ReactComponent as Delete } from './assets/icons/delete.svg';
-import { ReactComponent as Edit } from './assets/icons/edit.svg';
-import { ReactComponent as Reply } from './assets/icons/reply.svg';
-import Button from './components/Button';
-import Voting from './components/Voting';
+import Comment from './components/Comment';
 import Theme from './theme';
 
-function doNothing() {}
-
 function App() {
-  const [votes, setVotes] = useState(0);
-
   return (
     <Theme>
-      <h1>Interactive comments section</h1>
-      <Button
-        color="primary"
-        icon={Reply}
-        label="Reply"
-        onClick={doNothing}
-      />
-      <Button
-        color="secondary"
-        icon={Delete}
-        label="Delete"
-        onClick={doNothing}
-      />
-      <Button
-        color="primary"
-        icon={Edit}
-        label="Edit"
-        onClick={doNothing}
-      />
-      <Button
-        color="primary"
-        label="Send"
-        onClick={doNothing}
-        size="small"
-        variant="contained"
-      />
-      <Button
-        label="No, cancel"
-        onClick={doNothing}
-        size="big"
-        variant="contained"
-      />
-      <Button
-        color="secondary"
-        label="Yes, delete"
-        onClick={doNothing}
-        size="big"
-        variant="contained"
-      />
-      <Voting onVote={setVotes} votes={votes} />
+      <Comment />
     </Theme>
   );
 }
