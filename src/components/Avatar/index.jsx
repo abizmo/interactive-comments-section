48b68aaa3c15c892e1d2e629ac804c2f6e227412
Abián Izquierdo <abizmo@gmail.com>
2022-05-17 21:54:56 +0100

    Fix: Add support for aspect-ratio (test)

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const Picture = styled.picture`
+  aspect-ratio: 1;
+  width: 2rem;
+
+  @supports not (aspect-ratio: 1) {
+    &::before {
+      float: left;
+      padding-top: 100%;
+      content: "";
+    }
+
+    &::after {
+      display: block;
+      content: "";
+      clear: both;
+    }
+  }
+`;
 function Avatar({ user }) {
   return (
-    <picture style={{ aspectRatio: 1, width: '2rem' }}>
+    <Picture>
       <source srcSet={`${process.env.PUBLIC_URL}/${user.image.webp}`} type="image/webp" />
       <img style={{ width: '100%' }} src={`${process.env.PUBLIC_URL}/${user.image.png}`} alt={user.username} />
-    </picture>
+    </Picture>
   );
 }
 
